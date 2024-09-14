86f05bac5bc1543e1b22207c378163e560c0436a
HXXSTELLA <hxxstella@gmail.com>
2024-09-14 17:28:51 +0900

    v1.0.0

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://github.com/hxxstellaall/hxxstellaall_file/raw/main/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://github.com/HXXSTELLA/hxxstellaall_file/raw/main/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
